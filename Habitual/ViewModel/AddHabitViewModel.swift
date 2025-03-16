e5278e927199094f6a18556bc429c1d558e3f2df
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-03-16 11:44:43 +0000

    created the viewmodel class files

---
+++ Habitual/ViewModel/AddHabitViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddHabitViewModel.swift
+//  Habitual
+//
+//  Created by Taijaun Pitt on 16/03/2025.
+//
+
+import Foundation
+
+final class AddHabitViewModel {
+    
+}
---
+++ Habitual/ViewModel/MainListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainListViewModel.swift
+//  Habitual
+//
+//  Created by Taijaun Pitt on 16/03/2025.
+//
+
+import Foundation
+
+final class MainListViewModel {
+    
+}
